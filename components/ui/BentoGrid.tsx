5d2a97cd731151fde4184d6b3f3f8e6a91c83d48
Himanshu Singh Aswal <121888634+LAWSA07@users.noreply.github.com>
2025-03-07 23:07:26 +0530

    updates

--- components/ui/BentoGrid.tsx
+++ components/ui/BentoGrid.tsx
@@ -8,6 +8,7 @@ import { cn } from "@/utils/cn";
 import { BackgroundGradientAnimation } from "./GradientBg";
 import animationData from "@/data/confetti.json";
 import MagicButton from "./MagicButton";
+import Image from "next/image";
 
 export const BentoGrid = ({
   className,
@@ -89,7 +90,7 @@ export const BentoGridItem = ({
       <div className={`${id === 6 && "flex justify-center"} h-full`}>
         <div className="w-full h-full absolute">
           {img && (
-            <img
+            <Image
               src={img}
               alt={img}
               className={cn(imgClassName, "object-cover object-center")}
@@ -102,7 +103,7 @@ export const BentoGridItem = ({
           }`}
         >
           {spareImg && (
-            <img
+            <Image
               src={spareImg}
               alt={spareImg}
               className="object-cover object-center w-full h-full"
